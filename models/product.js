3227478abb5e89c1749e47eb330842b0116c4d97
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-05 20:52:41 +0530

    add

---
+++ models/product.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: [true, "Price must be provided"] },
+  featured: { type: Boolean, default: false },
+  rating: { type: Number, default: 4.0 },
+  createAt: { type: Date, default: Date.now() },
+  company: {
+    type: String,
+    enum: {
+      values: ["apple", "samsung", "dell", "mi","nokia"],
+      message: `${values} is not supported`,
+    },
+  },
+});
+
+module.exports = mongoose.model("Product",productSchema);
