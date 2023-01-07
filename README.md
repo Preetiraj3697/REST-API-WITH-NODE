d4cfe725c3d2d1b3cb7b9c162063901bbcb04a86
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-07 23:59:04 +0530

    add

---
+++ README.md
@@ -0,0 +1,13 @@
+## REST API 
+
+#### Technology use
+- Node.js
+- Express.js
+- Mongodb
+- Mongoose
+- Dotenv
+- Mongodb atlas
+- Thunder-client
+
+### link
+
--- controllers/products.js
+++ controllers/products.js
@@ -22,10 +22,16 @@ const getAllProducts = async(req,res) =>{
         let selectFix = select.split(",").join(" ");
         apiData = apiData.select(selectFix);
     }
+    //pagination=====================================================
+    let page = Number(req.query.page)  || 1;
+    let limit = Number(req.query.limit) || 3;
+    let skip = (page - 1)*limit;
+    apiData = apiData.skip(skip).limit(limit);
+    //==================================================================
     // console.log(queryObject)
     //req.query for searching ?company=apple&name=watch
     const myData = await apiData;
-     res.status(200).json({myData});
+     res.status(200).json({myData,nbHits:myData.length});
 }
 
 const getAllProductsTesting = async(req,res) =>{
